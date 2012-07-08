00e9e0265aabd68541d313aaa49b220b349c9f51
Olli Kankare <gildean@salaliitto.com>
2012-07-08 16:46:30 +0300

    Fix comments

--- httpredirect.js
+++ httpredirect.js
@@ -1,16 +1,26 @@
+// REDIRECT HTTP TO HTTPS
+
+// module depencies
+
 var express = require('express');
 var http = express.createServer();
 
-// set up a route to redirect http to https
 
+// set up a route to redirect http to https
+// the port argument replacing can be removed if default http and https ports are used
+// this is just for the demo, so the ports are just replaced instead
 
 http.get('*',function(req,res){  
 	var str = req.header('host');
-	str = str.replace( /:3005/g, ":3004" );
-    res.redirect('https://' + str )
+	str = str.replace( /:3005/g, ":3004" ); // remove this line if default ports or port forwarding is used
+    res.redirect('https://' + str + req.url )
 })
 
+
 // proxy up
+
 http.listen(3005);
 
+
+// export this thingamajig as a module
 module.exports.http = http;
