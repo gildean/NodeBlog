b195402ec0aaaec748a06b4e60b9ad2940602e1b
Olli K <olli@ankh-morpork.(none)>
2012-07-22 18:04:08 +0300

    update dbinfo

--- dbinfo.js
+++ dbinfo.js
@@ -3,6 +3,6 @@
 // ser your mongoDB database connection info here, the syntax is as follows: 
 // username:password@databaseurl.tld:port/databasename
 // if using localhost and default port, then you can just point to a db name (the database doesn't even have to exist!)
-var dbinfo = 'blogdb';
+var dbinfo = 'toomanymangos:TJs6hfjn3rhBhCU9j8b@ds033267.mongolab.com:33267/toomanymangos';
 
 module.exports.dbinfo = dbinfo;
