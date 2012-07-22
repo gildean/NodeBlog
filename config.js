f9da04238a00eb89216d631c64f45b61df0147ab
gildean <gildean@salaliitto.com>
2012-07-22 19:17:05 +0300

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // CONFIG
 exports.config = function(server, express){
 server.configure(function(){
-  server.set('views', __dirname + '/views');
+  server.set('views', __dirname + './views');
   server.set('view engine', 'jade');
   server.set('view options', { pretty: true });
   server.use(express.logger());
@@ -9,7 +9,7 @@ server.configure(function(){
   server.use(express.methodOverride());
   server.use(express.cookieParser());
   server.use(express.session({ secret: 'HJF839hrfsdjufFhj48w3hfsvfhushrfSVHydrg3547fa83yzcvwe' }));
-  server.use(express.static(__dirname + '/public'));
+  server.use(express.static(__dirname + './public'));
   server.use(server.router);
 });
 };
