3f2abdef540842428d49fdede143a8fa18ea9bfd
gildean <gildean@salaliitto.com>
2012-08-05 10:14:23 +0300

    Visual tweaks, cleaner errors

--- dbinfo.js
+++ dbinfo.js
@@ -14,7 +14,7 @@
 // username:password@databaseurl.tld:port/databasename
 // if using localhost with defaults, then you can just use a db name
 // note that the database doesn't even have to exist!
-var dbinfo = '192.168.81.5/123';
+var dbinfo = 'username:password@databaseurl.tld:port/databasename';
 
 
 // then just export this whole file as one module
